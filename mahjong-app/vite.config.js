c30c789aa5c92468b6eff59366ecaee3ff7b1f43
hanshsu <hanshsu@makalot.com.tw>
2026-04-20 14:53:31 +0800

    update

--- mahjong-app/vite.config.js
+++ mahjong-app/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 新增這一行
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // 新增這一行
-  ],
+  base: '/DomanChampionship/', 
+  plugins: [react()],
 })
